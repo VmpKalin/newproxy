9893fcc990e21d30da1ef79949f49940c174a541
Artur Goloyad <artik-17@mail.ru>
2024-06-01 18:52:32 +0200

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+const path = require('path')
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5001
+
+app.use(cors());
+
+// Proxy endpoint
+app.use('/api', createProxyMiddleware({
+  target: 'http://37.27.3.73/', // The server you want to proxy to
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // Remove /api prefix when forwarding
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    // Optionally, add headers or modify the request
+    proxyReq.setHeader('Origin', 'http://37.27.3.73/');
+  },
+}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
